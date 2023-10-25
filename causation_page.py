9166edf4383fa7e22721f68a5173ea204884942c
Nicholas Stepka <40475549+nstepka@users.noreply.github.com>
2023-10-25 11:55:39 -0500

    Update causation_page.py

--- causation_page.py
+++ causation_page.py
@@ -115,8 +115,9 @@ def display_relationships_definition():
         dot_representation = "digraph {\n"
         for relation in st.session_state.relationships:
             dot_representation += f'    "{relation[0]}" -> "{relation[1]}";\n'
-        dot_representation = dot_representation.rstrip()  # Remove trailing spaces or newlines
+        dot_representation = dot_representation.rstrip("; \n")  # Remove trailing spaces, newlines, or semicolons
         dot_representation += "\n}"  # Close the digraph with a newline and bracket
+
 
         # Debugging line to show the generated DOT representation
         st.write("Generated DOT representation:", dot_representation)
