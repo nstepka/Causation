2ec48cfa145bdfc086f5a7aa5e5b17c61fb20428
Nicholas Stepka <40475549+nstepka@users.noreply.github.com>
2024-01-12 09:12:30 -0600

    Update upload_data_page.py

--- upload_data_page.py
+++ upload_data_page.py
@@ -32,18 +32,7 @@ def upload_data():
     With a user-friendly interface, it facilitates various tasks from data upload to advanced analysis. 
     I hope this tool proves beneficial for your data science endeavors!
     """)
-    
-    st.markdown("""
-    ## About the Author
-    Hello! I'm **Nicholas Stepka**, a recent graduate in Computer Science from Tennessee State University, where I specialized in Data Science. I hold a strong passion for leveraging data to drive business solutions and innovation. My academic journey and professional experience have fostered a deep interest in business intelligence, machine learning, and statistical modeling.
 
-    My expertise lies in analyzing and interpreting complex datasets to uncover actionable insights. Through my senior project, "Airbnb Rent Prediction Model Based on Large Data," I have developed a keen understanding of how machine learning algorithms can be applied to real-world scenarios, especially in predictive analytics.
-
-    I am enthusiastic about exploring the intersection of technology and business, particularly how data-driven strategies can optimize performance and drive growth. My goal is to utilize my skills in Python, machine learning, and statistical modeling to contribute to the field of business intelligence and data science, creating impactful solutions for challenging problems.
-    
-    [Connect with me on LinkedIn](https://www.linkedin.com/in/nstepka/)""")
-
-    
 
     # Step 1: Upload CSV
     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
@@ -60,3 +49,16 @@ def upload_data():
         if st.button('Download Dataframe as CSV'):
             tmp_download_link = download_link(st.session_state.data, 'your_data.csv', 'Click here to download the data!')
             st.markdown(tmp_download_link, unsafe_allow_html=True)
+
+    
+    st.markdown("""
+    ## About the Author
+    Hello! I'm **Nicholas Stepka**, a recent graduate in Computer Science from Tennessee State University, where I specialized in Data Science. I hold a strong passion for leveraging data to drive business solutions and innovation. My academic journey and professional experience have fostered a deep interest in business intelligence, machine learning, and statistical modeling.
+
+    My expertise lies in analyzing and interpreting complex datasets to uncover actionable insights. Through my senior project, "Airbnb Rent Prediction Model Based on Large Data," I have developed a keen understanding of how machine learning algorithms can be applied to real-world scenarios, especially in predictive analytics.
+
+    I am enthusiastic about exploring the intersection of technology and business, particularly how data-driven strategies can optimize performance and drive growth. My goal is to utilize my skills in Python, machine learning, and statistical modeling to contribute to the field of business intelligence and data science, creating impactful solutions for challenging problems.
+    
+    [Connect with me on LinkedIn](https://www.linkedin.com/in/nstepka/)""")
+
+    
